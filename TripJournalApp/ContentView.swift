3a8f5db711c372bff0b863542263269a9a88cf07
mihmin98 <mihmin98@yahoo.com>
2021-04-03 12:00:10 +0200

    Initial commit

---
+++ TripJournalApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TripJournalApp
+//
+//  Created by user192166 on 4/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TripJournalApp/TripJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripJournalAppApp.swift
+//  TripJournalApp
+//
+//  Created by user192166 on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TripJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
