e7bf4840280f7aea04ec0f2a2bd418cee8559b11
mihmin98 <mihmin98@yahoo.com>
2021-04-03 19:05:59 +0200

    Added Constants file

---
+++ TripJournalApp/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TripJournalApp
+//
+//  Created by user192166 on 4/3/21.
+//
+
+import Foundation
+
+class Constants {
+    static let API_URL: String = "https://ab89a8a8dbef.ngrok.io/user/login"
+}
+	
