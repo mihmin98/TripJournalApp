15b1a4819cf76850e10b549a51ce2051d4e1ee15
user191232 <user191232@FF034.local>
2021-04-04 18:13:30 +0200

    add controller

---
+++ TripJournalApp/TripViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TripViewController.swift
+//  TripJournalApp
+//
+//  Created by user191232 on 4/4/21.
+//
+
+import UIKit
+
+class TripViewController: UIViewController {
+
+    @IBOutlet weak var tripPhoto: UIImageView!
+    @IBOutlet weak var tripName: UILabel!
+    @IBOutlet weak var tripLocation: UILabel!
+    @IBOutlet weak var tripCost: UILabel!
+    @IBOutlet weak var tripRating: UILabel!
+    @IBOutlet weak var tripDescription: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
